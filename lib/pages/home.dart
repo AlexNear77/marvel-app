745a1a5ec59054c4b9aaf949b44273030a631362
AlexNear77 <alexgomeznear@gmail.com>
2023-04-11 10:42:45 -0500

    adding home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,17 +13,45 @@ class Home extends StatefulWidget {
 class _Home extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.start,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Padding(padding: EdgeInsets.only(left: 20)),
+        SizedBox(height: 20),
         Text(
-          "Marvel Comics",
+          "Marvel Comic",
           style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-              color: Color.fromARGB(255, 168, 10, 10)),
-        )
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 20),
+        SizedBox(
+          width: 333,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+                "https://cdn.superaficionados.com/imagenes/personajes-destacados-marvel-og.jpg"),
+          ),
+        ),
+        SizedBox(height: 30),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            "¡Bienvenido a nuestra aplicación de comics de Marvel! Aquí encontrarás los mejores títulos y las historias más emocionantes. Mira nuestro catalogo!",
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 3,
+          ),
+        ),
+
+        /* Expanded(
+          child: ElevatedButton(
+            onPressed: () {
+              // Aquí puedes agregar la lógica para ir a la pantalla de comics
+            },
+            child: Text("Ver comics"),
+          ),
+        ), */
       ],
     );
   }
